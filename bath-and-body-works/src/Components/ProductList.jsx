1e61fddeccd52fee87ce7c30e105c9eb54f1dd50
sandeshjadhav5 <jadhavs932@gmail.com>
2022-12-14 18:31:57 +0530

    added

---
+++ bath-and-body-works/src/Components/ProductList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ProductList = () => {
+  return <div></div>;
+};
+
+export default ProductList;
--- bath-and-body-works/src/Pages/Products.jsx
+++ bath-and-body-works/src/Pages/Products.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Products = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Products Page</h1>
+    </div>
+  );
 };
 
 export default Products;
