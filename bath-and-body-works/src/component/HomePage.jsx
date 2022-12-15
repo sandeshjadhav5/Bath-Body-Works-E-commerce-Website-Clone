20812f248a8b2bd5c42b3f63a4e63de4fd20745c
shivam061095 <shivam061095@gmail.com>
2022-12-16 00:19:44 +0530

    Ssdd

--- bath-and-body-works/src/component/HomePage.jsx
+++ bath-and-body-works/src/component/HomePage.jsx
@@ -1,53 +1,131 @@
 import {Heading,Text,Box,Image,SimpleGrid,Button} from '@chakra-ui/react'
 const Home=()=>{
     return <>
-        <Box width="90%" ml="100px">
-  <Image src='https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw6577ea42/images/Christmas2022/ee325-soaps_c2_16x9.jpg?yocs=s_' alt='Dan Abramov' />
-</Box>
-<Box height="300px" bg="lightgray"  width="85%" margin="auto" mt="100px">
-    <Heading pt="10px">TODAY`S TOP OFFERS</Heading>
-<SimpleGrid columns={4} spacing={10} textAlign="centre" >
-  <Box bg="white" height='180px'><Heading as="h6" color={'blue.900'} fontSize="2xl" >Today only!</Heading>
-  <Text>All Hand Soaps</Text>
-  <Heading as="h4" >$3.25</Heading>
-  <Text>limit 20 with code :MORESUDS</Text>
-  <Button bg="black" borderRadius={0} color="white" width="60%">SHOP</Button></Box>
-  <Box bg='white' height='180px'></Box>
-  <Box bg='white' height='180px'></Box>
-  <Box bg='white' height='180px'></Box>
+  <Box width="85%" margin="auto" >
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw6577ea42/images/Christmas2022/ee325-soaps_c2_16x9.jpg?yocs=s_" />
+
+  </Box>
+
+  <Box  height="300px" mt="50px" bg="gray.200" width="85%" ml="100px">
+    <Heading as="h6" pt="20px">TODAY`S TOP OFFERS</Heading>
+    <SimpleGrid columns={5} spacing={10} mt="10px" pl="30px">
+  <Box bg='white' height='170px' ></Box>
+  <Box bg='white' height='170px'></Box>
+  <Box bg='white' height='170px'></Box>
+  <Box bg='white' height='170px'></Box>
+  <Box bg='white' height='170px'></Box>
 </SimpleGrid>
-</Box>
-<Box height="400px" mt="100px" width="85%" ml="100px">
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwd1c59e9e/images/Christmas2022/cndl_holtrads-wb_c1_hm.gif?yocs=s_"></Image>
-    <Image  pt="100px"src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw315b64eb/images/Christmas2022/bc_core-gifting_c2_hm.jpg?yocs=s_"></Image>
-   <Heading pt="50px">SHOP BY CATEGORY</Heading>
-    <SimpleGrid columns={6}spacing={10} pt="40px">
-  <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw90169280/images/Christmas2022/bc_dream-bright_c1_vn.jpg?yocs=o_s_"></Image>
-    <Text>Shop Body Care</Text>
+  </Box>
+  <Box width="85%" ml="100px" mt="50px" >
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwd1c59e9e/images/Christmas2022/cndl_holtrads-wb_c1_hm.gif?yocs=s_" />
+
+  </Box>
+  <Box width="85%" mt="40px" ml="100px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw315b64eb/images/Christmas2022/bc_core-gifting_c2_hm.jpg?yocs=s_" />
+  </Box>
+  <Box mt="100px">
+  <SimpleGrid columns={6} spacing={10} width="85%" ml="100px">
+  <Box  height='80px'>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw90169280/images/Christmas2022/bc_dream-bright_c1_vn.jpg?yocs=o_s_" />
   </Box>
   <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw9c3dac1d/images/Christmas2022/cndl_holtrads-wb_c1_vn.jpg?yocs=o_s_"></Image>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw9c3dac1d/images/Christmas2022/cndl_holtrads-wb_c1_vn.jpg?yocs=o_s_" />
   </Box>
   <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw31e8f9db/images/Christmas2022/diff_innovation_c1_vn.jpg?yocs=o_s_"></Image>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw31e8f9db/images/Christmas2022/diff_innovation_c1_vn.jpg?yocs=o_s_" />
   </Box>
   <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw8ac572be/images/Christmas2022/sp_holtrads-gifting_c1_vn.jpg?yocs=o_s_"></Image>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw8ac572be/images/Christmas2022/sp_holtrads-gifting_c1_vn.jpg?yocs=o_s_" />
   </Box>
   <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwcf3b1b26/images/Christmas2022/bc_mens-sfl-blktie_c1_vn.jpg?yocs=o_s_"></Image>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwcf3b1b26/images/Christmas2022/bc_mens-sfl-blktie_c1_vn.jpg?yocs=o_s_" />
   </Box>
   <Box  height='180px'>
-    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw2fdbe2d9/xcat_stg-gift-sets_c1_vn.jpg?yocs=o_s_"></Image>
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw2fdbe2d9/xcat_stg-gift-sets_c1_vn.jpg?yocs=o_s_" />
   </Box>
 </SimpleGrid>
+  </Box>
+  <Box width="85%" mt="30px"ml="100px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw126cc0be/images/Christmas2022/xcat_hp-gifting-zone_c1_hb.jpg?yocs=s_" />
+  </Box>
 
-<Image pt="40px"src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw126cc0be/images/Christmas2022/xcat_hp-gifting-zone_c1_hb.jpg?yocs=s_"></Image>
-
+  <Box height="300px" width="80%" ml="130px">
+  <SimpleGrid columns={5} spacing={10} mt="30px">
+  <Button  height='55px'color="white" bg="blackAlpha.800" borderRadius={0}>STOCKING STUFFERS</Button>
+  <Button  height='55px'color="white" bg="blackAlpha.800" borderRadius={0}>GIFT SETS</Button>
+  <Button  height='55px'color="white" bg="blackAlpha.800" borderRadius={0}>GIFTS UNDER $15</Button>
+  <Button  height='55px'color="white" bg="blackAlpha.800" borderRadius={0}>GIFTS FOR HER</Button>
+  <Button  height='55px'color="white" bg="blackAlpha.800" borderRadius={0}>GIFTS FOR HIM</Button>
+  
+</SimpleGrid>
+<Box>
+<Button mt="30px" height='55px'color="white"  bg="blackAlpha.800" borderRadius={0}>GIFT CARDS</Button>
 </Box>
 
 
+  </Box>
+  <Box  width="85%" ml="100px" >
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw481d974e/images/Fall2022/xcat_moxy-launch_fa3_gh.gif?yocs=s_" />
+  </Box>
+  <Box mt="20px">
+  <SimpleGrid columns={5} spacing={10} mt="30px" ml="550px">
+  <Button  height='50px'color="white" bg="blackAlpha.800" borderRadius={0}>LEARN MORE</Button>
+  <Button  height='50px'color="white" bg="blackAlpha.800" borderRadius={0}>SHOP MOXY</Button>
+
+</SimpleGrid>
+  
+  </Box>
+  <Box mt="30px">
+    <Heading>MORE GOOD THINGS,THIS WAY</Heading>
+  <SimpleGrid columns={6} spacing={10} width="85%" ml="100px" mt="30px">
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwb9d60d44/images/Fall2022/boc-launch-prospects_vn_0.png?yocs=o_s_" />
+    <Text>join the VIPs</Text>
+
+  </Box>
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dwae0faf07/images/Christmas2022/xcat_bopisgeneric_c1_vn.png?yocs=o_s_" />
+    <Text>Shop your way</Text>
+  </Box>
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw644e724c/images/Spring2022/xcat_mwts-sms_sp2_vn.jpg?yocs=o_s_" />
+    <Text >Sign up for text</Text>
+  </Box>
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw41a3328c/images/Spring2022/xcat_mwts-email_sp2_vn.jpg?yocs=o_s_" />
+
+    <Text>Sign up for emails</Text>
+  </Box>
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw611329aa/images/Spring2022/xcat_mwts-autorefresh_sp2_vn.png?yocs=o_s_" />
+    <Text>check out Auto Refresh</Text>
+  </Box>
+  <Box  height='200px' pt="20px">
+    <Image src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.21a/on/demandware.static/-/Sites-BathAndBodyWorks-Library/default/dw4dd3c5f1/images/Spring2022/xcat_mwts-returns_sp2_vn.jpg?yocs=o_s_" />
+    <Text>See Return Policy for details and conditions
+</Text>
+  </Box>
+</SimpleGrid>
+  </Box>
+  <Box>
+  <SimpleGrid columns={4} spacing={10} width="85%" ml="100px" mt="100px">
+  <Box bg='tomato' height='200px'>
+    <Image src="https://images.dashhudson.com/aHR0cHM6Ly9jZG4uZGFzaGh1ZHNvbi5jb20vbWVkaWEvZnVsbC8xNjcxMDQ4MDgwLjE2Mjc1MTQyNDQ2Ny5qcGVn.jpg?w=640&h=640&fit=cover" />
+  </Box>
+  <Box bg='tomato' height='200px'>
+    <Image src="https://images.dashhudson.com/aHR0cHM6Ly9jZG4uZGFzaGh1ZHNvbi5jb20vbWVkaWEvZnVsbC8xNjcxMDMwMzg3LjI2MjU1MDQyNTI5MS5qcGVn.jpg?w=640&h=640&fit=cover" />
+
+  </Box>
+  <Box bg='tomato' height='200px'>
+    <Image src="https://images.dashhudson.com/aHR0cHM6Ly9jZG4uZGFzaGh1ZHNvbi5jb20vbWVkaWEvZnVsbC8xNjcwOTYxNjgyLjg3OTUxNTQ0NDg1Ni5qcGVn.jpg?w=640&h=640&fit=cover"/>
+  </Box>
+  <Box bg='tomato' height='200px'>
+    <Image src="https://images.dashhudson.com/aHR0cHM6Ly9jZG4uZGFzaGh1ZHNvbi5jb20vbWVkaWEvZnVsbC8xNjcwOTQzNzA3LjQ0NTgwOTY4NDUzMC5qcGVn.jpg?w=640&h=640&fit=cover" />
+  </Box>
+  
+</SimpleGrid>
+  </Box>
+       
 
 
 
