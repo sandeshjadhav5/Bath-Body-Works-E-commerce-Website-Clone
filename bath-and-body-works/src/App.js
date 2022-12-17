a98947b24ba4772765202b2266754ac0c62ba38f
sandeshjadhav5 <jadhavs932@gmail.com>
2022-12-17 15:08:18 +0530

    added

--- bath-and-body-works/src/App.js
+++ bath-and-body-works/src/App.js
@@ -3,14 +3,11 @@ import "./App.css";
 import Navbar from "./Components/Navbar";
 import MainRoutes from "./Pages/MainRoutes";
 
-
-
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      {/* <Navbar/> */}
       <MainRoutes />
-
     </div>
   );
 }
