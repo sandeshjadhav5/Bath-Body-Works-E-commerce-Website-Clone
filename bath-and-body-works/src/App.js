aec8691fbf4cf47870d8d98745eac14efee42033
sandeshjadhav5 <jadhavs932@gmail.com>
2022-12-19 10:24:33 +0530

    added final

--- bath-and-body-works/src/App.js
+++ bath-and-body-works/src/App.js
@@ -6,7 +6,7 @@ import MainRoutes from "./Pages/MainRoutes";
 function App() {
   return (
     <div className="App">
-      {/* <Navbar/> */}
+      <Navbar />
       <MainRoutes />
     </div>
   );
